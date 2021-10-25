f7635260f2a0d43a065613af860878b22daa92af
Prakash Muthusamy <prakash.muthusamy@cesltd.com>
2021-10-25 16:21:04 +0530

    uploaded the codes

---
+++ README.md
@@ -0,0 +1,33 @@
+# Problem :
+Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You 
+must also not convert the inputs to integers directly.
+
+
+## Example 1:
+Input: num1 = "11", num2 = "123"
+Output: "134"
+
+## Example 2:
+Input: num1 = "456", num2 = "77"
+Output: "533"
+
+## Example 3:
+Input: num1 = "0", num2 = "0"
+Output: "0"
+
+
+### [leetcode link]("https://leetcode.com/problems/add-strings/")
+
+
+## Algorithm :
+
+- step1 : Get the two strings as named num1 and num2
+- step2 : convert the two strings into the numbers and stored variables a and b
+- step3 : add those numbers and stored into the variable add
+- step4 : add a zero string in front of the add
+- step5 : iterate the variable add with for loop
+- step6 : stored the values in new variable
+- step7 : print the trimmed value 
+
+
---
+++ assets/scripts/index.js
@@ -0,0 +1,164 @@
+// Input: num1 = "11", num2 = "123"
+// Output: "134"
+// Example 2:
+
+// Input: num1 = "456", num2 = "77"
+// Output: "533"
+// Example 3:
+
+// Input: num1 = "0", num2 = "0"
+// Output: "0"
+
+// Input: num1 = "9333852702227987" , num2 = "85731737104263"
+// Output: "9419584439332250"
+
+
+//Solution 1:
+
+var num1 = "456";
+var num2 = "77";
+
+const a = Number(num1); // or parseInt(num1)
+const b = Number(num2);
+let add = a + b;
+out = "0" + add;
+var sum = "";
+for (let i = 1; i < out.length; i++) {
+    sum += out[i] + "";
+}
+console.log(sum.trim());
+
+
+
+
+
+
+
+
+
+
+//Solution 2:
+
+
+// var num1 = "11";
+// var num2 = "123";
+// Fstr = "";
+
+// var a = [];
+// var carry = 0;
+// const Lnum1 = num1.length
+// const Lnum2 = num2.length
+
+// if (Lnum1 > Lnum2) {
+//     Flen = Lnum1 - Lnum2;
+
+//     for (j = 0; j < Flen; j++) {
+//         Fstr += "0"
+//     }
+//     Fstr += num2;
+
+//     for (let i = Lnum1 - 1; i >= 0; i--) {
+//         let x = parseInt(num1[i]);
+//         let y = parseInt(Fstr[i]);
+
+//         let sum = x + y + carry;
+//         if (i === 0) {
+//             if (sum > 10) {
+//                 a.push(sum - 10);
+//                 a.push(1);
+
+
+//             } else if (sum === 10) {
+//                 a.push(0)
+//                 a.push(1);
+//             } else {
+//                 a.push(sum);
+//             }
+
+//         } else {
+
+//             if (sum > 10) {
+//                 a.push(sum - 10);
+//                 carry = 1;
+//             } else if (sum === 10) {
+//                 a.push(0)
+//                 carry = 1;
+//             } else {
+//                 a.push(sum);
+//             }
+//         }
+//     }
+
+// } else if (Lnum1 === Lnum2) {
+
+//     for (let i = Lnum1 - 1; i >= 0; i--) {
+
+//         let x = parseInt(num1[i]);
+//         let y = parseInt(num2[i]);
+//         let sum = x + y + carry;
+
+//         if (i === 0) {
+//             if (sum > 10) {
+//                 a.push(sum - 10);
+//                 a.push(1);
+
+
+//             } else if (sum === 10) {
+//                 a.push(0)
+//                 a.push(1);
+//             } else {
+//                 a.push(sum);
+//             }
+
+//         } else {
+
+//             if (sum > 10) {
+//                 a.push(sum - 10);
+//                 carry = 1;
+
+//             } else if (sum === 10) {
+//                 a.push(0)
+//                 carry = 1;
+//             } else {
+//                 a.push(sum);
+//             }
+//         }
+//     }
+
+// } else {
+//     if (Lnum1 < Lnum2) {
+
+//         Flen = Lnum2 - Lnum1;
+//         for (j = 0; j < Flen; j++) {
+//             Fstr += "0"
+//         }
+//         Fstr += num1;
+
+
+//         for (let i = Lnum2 - 1; i >= 0; i--) {
+//             let x = parseInt(Fstr[i]);
+//             let y = parseInt(num2[i]);
+
+//             let sum = x + y + carry;
+
+//             if (sum > 10) {
+//                 a.push(sum - 10);
+//                 carry = 1;
+//             } else if (sum === 10) {
+//                 a.push(0)
+//                 carry = 1;
+//             } else {
+//                 a.push(sum);
+//             }
+//         }
+
+//     }
+// }
+
+// var resLen = a.length;
+// var res = "";
+// for (let i = resLen - 1; i >= 0; i--) {
+//     res += a[i] + "";
+// }
+
+// console.log(res.trim())
